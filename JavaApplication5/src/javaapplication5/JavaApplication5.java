7f114ff2699b94f3e72b30a0d8ae236e80d4c00e
MagdielPaulo <81927049+MagdielPaulo@users.noreply.github.com>
2026-01-29 05:10:53 -0300

    Add files via upload

---
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,15 @@
+package javaapplication5;
+
+public class JavaApplication5 {
+
+    public static void main(String[] args) {
+        for (int x = 1; x < 10; x++) {
+            if (x % 4 != 0 && x % 2 == 0) {
+                for (int y = 1; y < 5; y++) {
+                System.out.println(y);
+                }
+            }
+        }
+        
+    }
+}
