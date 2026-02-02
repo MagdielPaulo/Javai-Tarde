1d4bc42a787272f3274422203fa4b0cc767d72d0
MagdielPaulo <81927049+MagdielPaulo@users.noreply.github.com>
2026-02-02 12:39:30 -0300

    poo

---
+++ poo/src/poo/Cao.java
@@ -0,0 +1,14 @@
+
+package poo;
+
+public class Cao {
+    String nome;
+    String cor;
+    int idade;
+    double peso;
+    
+    public void Anda(){
+        System.out.println("Estou andando...");
+    }
+    
+}
---
+++ poo/src/poo/Poo.java
@@ -0,0 +1,13 @@
+
+package poo;
+
+public class Poo {
+
+
+    public static void main(String[] args) {
+        Cao cachorro = new Cao();
+        cachorro.nome = "Braik";
+        cachorro.Anda();
+    }
+    
+}
